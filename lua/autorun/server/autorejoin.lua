2da11197cd5b1999e03b5531c12e313141023d67
Simon Schick <SimonSchick@users.noreply.github.com>
2016-01-22 20:08:08 +0100

    Forgot flags

--- lua/autorun/server/autorejoin.lua
+++ lua/autorun/server/autorejoin.lua
@@ -3,10 +3,11 @@ if game.SinglePlayer() then
 	return
 end
 
+local flags = {FCVAR_USERINFO, FCVAR_REPLICATED}
 CreateConVar("timeout_notice_might_have_crashed", 5, flags, "When to show the first timeout message")
 CreateConVar("timeout_notice_probably_restarting", 20, flags, "When to show the second timeout message")
 CreateConVar("timeout_notice_countdown", 30, flags, "When to show the countdown")
- CreateConVar("timeout_notice_rejoining", 45, flags, "When to rejoin")
+CreateConVar("timeout_notice_rejoining", 45, flags, "When to rejoin")
 
 AddCSLuaFile("autorun/client/autorejoin.lua")
 AddCSLuaFile("gui/timeoutnotice.lua")
