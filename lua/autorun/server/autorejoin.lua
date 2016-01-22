11c1f1d8ca4e23944617dec6f847713c031db51e
Simon Schick <SimonSchick@users.noreply.github.com>
2016-01-22 20:06:40 +0100

    Added convars.

--- lua/autorun/server/autorejoin.lua
+++ lua/autorun/server/autorejoin.lua
@@ -3,6 +3,11 @@ if game.SinglePlayer() then
 	return
 end
 
+local mightHaveCrashedConVar = CreateConVar("timeout_notice_might_have_crashed", 5, flags, "When to show the first timeout message")
+local probablyRestartingConVar = CreateConVar("timeout_notice_probably_restarting", 20, flags, "When to show the second timeout message")
+local countdownConVar = CreateConVar("timeout_notice_countdown", 30, flags, "When to show the countdown")
+local rejoinxyingConVar = CreateConVar("timeout_notice_rejoining", 45, flags, "When to rejoin")
+
 AddCSLuaFile("autorun/client/autorejoin.lua")
 AddCSLuaFile("gui/timeoutnotice.lua")
 AddCSLuaFile("util/systimer.lua")
