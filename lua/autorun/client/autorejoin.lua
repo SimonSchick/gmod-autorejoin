fddaf11602bc31ecc9b6e9b10e0ae06eb930aee3
Simon Schick <SimonSchick@users.noreply.github.com>
2016-01-22 20:07:52 +0100

    Moved actual rejoin logic into this file, added convars.

--- lua/autorun/client/autorejoin.lua
+++ lua/autorun/client/autorejoin.lua
@@ -2,6 +2,10 @@ if game.SinglePlayer() then
 	return
 end
 
+local mightHaveCrashedConVar = GetConVar("timeout_notice_might_have_crashed")
+local probablyRestartingConVar = GetConVar("timeout_notice_probably_restarting")
+local countdownConVar = GetConVar("timeout_notice_countdown")
+local rejoinxyingConVar = GetConVar("timeout_notice_rejoining")
 
 include("gui/timeoutnotice.lua")
 include("util/systimer.lua")
@@ -10,6 +14,12 @@ RunConsoleCommand('cl_timeout', 9999)
 
 function makeNotice()
 	local notice = vgui.Create("TimeoutNotice")
+	notice:SetNoticeTimeouts({
+		mightHaveCrashed = mightHaveCrashedConVar:GetInt(),
+		probablyRestarting = probablyRestartingConVar:GetInt(),
+		countdown = countdownConVar:GetInt(),
+		rejoining = rejoiningConVar:GetInt()
+	})
 	notice:SetSize(350, 120)
 	notice:Center()
 	notice:SetVisible(false)
@@ -26,6 +36,9 @@ function onTimeout()
 	if not isTimingOut then
 		notice = notice or makeNotice()
 		notice:StartNotice()
+		systimer.Create("AutoRejoin-Rejoin", rejoiningConVar:GetInt(), 1, function()
+			LocalPlayer():ConCommand("retry")
+		end)
 		isTimingOut = true
 	end
 end
@@ -43,6 +56,7 @@ net.Receive("AutoRejoinHeartBeat", function()
 	systimer.Adjust("AutoRejoin", 2, 0, onTimeout)
 	if isTimingOut and notice then
 		notice:EndNotice()
+		systimer.Remove("AutoRejoin-Rejoin")
 		isTimingOut = false
 	end
 end)
